08bd57d70d7520f310d274eb1a0cba7c881faec3
kierepkae <kierepkaedyta@gmail.com>
2019-07-17 20:33:57 +0100

    add first test for Task

--- rspec/task_spec.rb
+++ rspec/task_spec.rb
@@ -1,3 +1,9 @@
+require_relative '../task'
 describe Task do
-  it "cr"
+  describe "#initialize " do
+    it "creates a task with value as an argument" do
+      task = Task.new("make shopping")
+      expect(task.value).to eq("make shopping")
+    end
+  end
 end
