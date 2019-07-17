4022153f8869d699c4e8c5fba2d6f0c542831bec
kierepkae <kierepkaedyta@gmail.com>
2019-07-17 20:38:48 +0100

    add second test for default done value

--- rspec/task_spec.rb
+++ rspec/task_spec.rb
@@ -5,5 +5,9 @@ describe Task do
       task = Task.new("make shopping")
       expect(task.value).to eq("make shopping")
     end
+    it "sets the default value of done as false" do
+      task = Task.new("go for a walk")
+      expect(task.done).to eq(false)
+    end
   end
 end
