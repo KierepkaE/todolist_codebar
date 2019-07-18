1670f43bcf4bc90415a4b86fe6d8c64a250b2cef
kierepkae <kierepkaedyta@gmail.com>
2019-07-18 17:34:33 +0100

    correct spaces in task_spec.rb

--- rspec/task_spec.rb
+++ rspec/task_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../task'
 
 describe Task do
+
   describe "#initialize " do
     let(:value) {"cook"}
     let(:task) {Task.new(value)}
@@ -8,22 +9,28 @@ describe Task do
     it "creates a task with value as an argument" do
       expect(task.value).to eq(value)
     end
+
     it "sets the default value of done as false" do
       expect(task.done).to eq(false)
     end
+
   end
 
   describe "#set_as_done" do
+
     it "changes the value of done to true" do
       task = Task.new("eat pizza")
       task.set_as_done
       expect(task.done).to eq(true)
     end
+
   end
 
   describe "#done?" do
+
     let(:value) {"cook"}
     let(:task) {Task.new(value)}
+
     it "return default value of done" do
     expect(task.done?).to eq(false)
     end
@@ -32,21 +39,28 @@ describe Task do
       task.set_as_done
       expect(task.done).to eq(true)
     end
+
   end
 
   describe "#set_as_pending" do
     let(:value) {"cook"}
     let(:task) {Task.new(value)}
+
    it "sets task as pending" do
     task.set_as_pending
     expect(task.done).to eq false
    end
+
   end
+
   describe "#to_s" do
+
     let(:value) {"cook"}
     let(:task) {Task.new(value)}
     it "returns value as a string" do
       expect(task.to_s).to eq task.to_s
     end
-end
+
+  end
+
 end
